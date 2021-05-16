bb045a4e487b8d1f6de98dbbb7dd44b6f21af8c7
Surbhi Suman <45829819+surbhimgr@users.noreply.github.com>
2021-05-16 10:54:04 +0530

    Add files via upload

---
+++ code (2).py
@@ -0,0 +1,42 @@
+class Solution(object):
+    def isNumber(self, s):
+        #define DFA state transition tables
+        states = [{},
+                 # State (1) - initial state (scan ahead thru blanks)
+                 {'blank': 1, 'sign': 2, 'digit':3, '.':4},
+                 # State (2) - found sign (expect digit/dot)
+                 {'digit':3, '.':4},
+                 # State (3) - digit consumer (loop until non-digit)
+                 {'digit':3, '.':5, 'e':6, 'blank':9},
+                 # State (4) - found dot (only a digit is valid)
+                 {'digit':5},
+                 # State (5) - after dot (expect digits, e, or end of valid input)
+                 {'digit':5, 'e':6, 'blank':9},
+                 # State (6) - found 'e' (only a sign or digit valid)
+                 {'sign':7, 'digit':8},
+                 # State (7) - sign after 'e' (only digit)
+                 {'digit':8},
+                 # State (8) - digit after 'e' (expect digits or end of valid input) 
+                 {'digit':8, 'blank':9},
+                 # State (9) - Terminal state (fail if non-blank found)
+                 {'blank':9}]
+        currentState = 1
+        for c in s:
+	    if c=='E':
+		c='e'   #convert E to e as it is also acceptable
+            # If char c is of a known class set it to the class name
+            if c in '0123456789':
+                c = 'digit'
+            elif c == ' ':
+                c = 'blank'
+            elif c in '+-':
+                c = 'sign'
+            # If char/class is not in our state transition table it is invalid input
+            if c not in states[currentState]:
+                return False
+            # State transition
+            currentState = states[currentState][c]  #checking the sub dictionary whose index is currentState 
+        # The only valid terminal states are end on digit, after dot, digit after e, or white space after valid input    
+        if currentState not in [3,5,8,9]:
+            return False
+        return True
