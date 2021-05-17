72f2b70834e80e5e8147a099a54403a106bbaa31
Surbhi Suman <45829819+surbhimgr@users.noreply.github.com>
2021-05-17 10:28:03 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the saveThePrisoner function below.
+def saveThePrisoner(n, m, s):
+    if n<m:
+        c=m-((n+1)-s)
+        res=c%n
+        if res==0:
+            res=n
+    else:
+        res=s+(m-1)
+        if res>n:
+            res=res%n
+    return res
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        nms = input().split()
+
+        n = int(nms[0])
+
+        m = int(nms[1])
+
+        s = int(nms[2])
+
+        result = saveThePrisoner(n, m, s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
